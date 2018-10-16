2786e8fb1f1bd8c806052a34500bda9e7129f9b3
pahuja991 <rp824@njit.edu>
2018-10-16 11:30:34 -0400

    Feature:setting directories

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    About
                 </div>
 
                 <div class="links">
