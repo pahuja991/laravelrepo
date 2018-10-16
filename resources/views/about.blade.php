ef799471050d7b3bf2eba3ef112fa4598c6da3af
pahuja991 <rp824@njit.edu>
2018-10-16 18:35:08 -0400

    Fix:Added Home page content

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout.content')
-@section('content')
-    <div class="form-group">
-        <h1 class="text-secondary">This is About Page</h1>
-    </div>
-@stop
--- resources/views/includes/header1.blade.php
+++ resources/views/includes/header1.blade.php
@@ -1,21 +1,6 @@
-<!-- header file contains navbar -->
-
-@extends('includes.head')
-<header>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-danger fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">First laravel homework</a>
-
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item ">
-                        <a class="nav-link" href="/home">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
+@extends('layouts.content')
+@section('content')
+    <div class="form-group">
+        <h1 class="text-secondary">This is Home Page</h1>
+    </div>
+@stop
