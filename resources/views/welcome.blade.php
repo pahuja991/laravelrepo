caecd427ac3d3801d9b7816ed94a45a94f9de83f
pahuja991 <rp824@njit.edu>
2018-10-16 19:42:13 -0400

    Fix:Fix the content of routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel project</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to the homepage
+                    Welcome to homepage
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,15 @@
+
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.structure');
 });
 
+Route::get('/home', function () {
+    return view('pages.homepage');
+});
 
 Route::get('/about', function () {
-    return view('about');
+    return view('pages.Aboutpage');
 });
